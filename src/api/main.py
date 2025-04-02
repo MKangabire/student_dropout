35926f1a48827d0772594218ca906c7cea035ec8
MKangabire <m.kangabire@alustudent.com>
2025-04-02 10:49:34 +0200

    deployment of frontend

--- src/api/main.py
+++ src/api/main.py
@@ -20,7 +20,7 @@ app = FastAPI()
 # Add CORS middleware to allow frontend requests
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Frontend URL
+    allow_origins=["https://studentdropout-qugy2gmua-mkangabires-projects.vercel.app"],  # Frontend URL
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
